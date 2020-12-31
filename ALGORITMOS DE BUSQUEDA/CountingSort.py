bacd79efec8223c3deaed034b1e4fe451da530cb
Diego-Santiago-Gutierrez <76793627+Diego-Santiago-Gutierrez@users.noreply.github.com>
2020-12-31 16:23:50 -0600

    My Searching Algorithms
    All of these includes a basic example with an A array with 7 elements.

---
+++ ALGORITMOS DE BUSQUEDA/CountingSort.py
@@ -0,0 +1,30 @@
+def countingSort(arr):
+    k = max(arr)
+    n = len(arr)
+
+    # Arreglo de índices
+    idxArr = [0] * (k + 1)
+    # Arreglo donde se colocarán los elementos ordenados
+    final = [0] * n
+
+    # Colocar numero de ocurrenciss de cada elemento
+    for num in arr:
+        idxArr[num] += 1
+
+    # Calcular indices
+    for i in range(1, k + 1):
+        idxArr[i] += idxArr[i - 1]
+
+    # Colocar los elementos en sus respectivas posiciones
+    for j in range(n - 1, -1, -1):
+        toAdd = arr[j]
+        idxArr[toAdd] -= 1
+        final[idxArr[toAdd]] = toAdd
+
+    return final
+
+
+A= [9, 0, 4, 6, 2, 1, 10]
+print("Lista aleatoria: ", A)
+C = countingSort(A)
+print("Lista arreglada: ", C)
